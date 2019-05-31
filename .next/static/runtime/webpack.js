ff4d4aee00a2a5a7daa85760afa39a16dca55c6b
suchov <artem.suchov@gmail.com>
2019-05-31 10:49:59 +0300

    cleen up

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bbeb53315e7a866091d8";
+/******/ 	var hotCurrentHash = "60730a506e1228688ec0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
